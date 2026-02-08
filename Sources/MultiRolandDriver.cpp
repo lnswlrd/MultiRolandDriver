51de34d9d56ba074704dd0dca2b0f23fde52d51f
Linus Wileryd <linus@cutup.se>
2026-02-08 16:16:21 +0100

    v1.1.0: Add 8 multi-port and composite devices

--- Sources/MultiRolandDriver.cpp
+++ Sources/MultiRolandDriver.cpp
@@ -576,6 +576,6 @@ void *MultiRolandDriverCreate(CFAllocatorRef /*alloc*/, CFUUIDRef typeUUID)
 
     CFPlugInAddInstanceForFactory(state->factoryID);
 
-    os_log(sLog, "MultiRolandDriver v1.0.13 loaded");
+    os_log(sLog, "MultiRolandDriver v1.1.0 loaded");
     return state;
 }
